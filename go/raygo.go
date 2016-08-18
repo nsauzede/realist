7175eba73ae798d98e73b927d81b8ed8770bf317
Nicolas Sauzede <nsauzede@laposte.net>
2016-08-19 00:11:02 +0200

    improve go version to output something close to raypy; implement dot product

--- go/raygo.go
+++ go/raygo.go
@@ -5,23 +5,22 @@ import (
 	"vec"
 )
 
-var w = 3
-var h = 3
+var w = 10
+var h = 10
 var nobj = 4
 
-var s, o vec.Vector
+var s, e vec.V3
 
 func Solvetri(a, b, c float32) (sol int, t1, t2 float32) {
 	return
 }
-func Intersec(s, o, v vec.Vector) (t float32) {
+func Intersec(s, o, v vec.V3) (t float32) {
 	t = v.Dot(v)
 	return
 }
 
-func Trace() (r, g, b float32) {
+func Trace(o, v vec.V3) (r, g, b float32) {
 	for i := 0; i < nobj; i++ {
-		var v vec.Vector
 		Intersec(s, o, v)
 	}
 	return
@@ -30,17 +29,20 @@ func Trace() (r, g, b float32) {
 func Render() {
 	for j := 0; j < h; j++ {
 		for i := 0; i < w; i++ {
-			Trace()
+			var v vec.V3
+			r, g, b := Trace(e, v)
+			fmt.Printf(" %3.f,%3.f,%3.f", 100*r, 100*g, 100*b)
 		}
+		fmt.Println()
 	}
 }
 
 func Draw() {
 	for j := 0; j < h; j++ {
 		for i := 0; i < w; i++ {
-			fmt.Print(".")
+//			fmt.Print(".")
 		}
-		fmt.Println()
+//		fmt.Println()
 	}
 }
 
--- go/src/vec/vec.go
+++ go/src/vec/vec.go
@@ -1,9 +1,9 @@
 package vec
 
-type Vector struct {
+type V3 struct {
 	X, Y, Z float32
 }
 
-func (v Vector) Dot(ov Vector) float32 {
-	return 0
+func (v V3) Dot(ov V3) float32 {
+	return v.X * ov.X + v.Y * ov.Y + v.Z * ov.Z
 }
