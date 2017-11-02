2cdccdd44fd276688a249fdac85bfae3a7a3c801
YruamaLairba <Yruama_Lairba@hotmail.com>
2017-11-02 20:48:33 +0100

    implement add trait for V3

--- rayrust/src/lib.rs
+++ rayrust/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 pub struct V3 {
     x: f64,
     y: f64,
@@ -10,6 +12,17 @@ impl V3 {
             x: self.y * other.z - self.z * other.y,
             y: self.z * other.x - self.x * other.z,
             z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
+
+impl Add for V3 {
+    type Output = V3;
+    fn add(self, other: V3) -> V3 {
+        V3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
         }
     }
 }
@@ -27,5 +40,15 @@ mod tests {
         assert_eq!(cross.x, 0f64);
         assert_eq!(cross.y, 0f64);
         assert_eq!(cross.z, 0f64);
+    }
+
+    #[test]
+    fn test_add() {
+        let a =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let b =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let sum = a + b;
+        assert_eq!(sum.x, 4f64);
+        assert_eq!(sum.y, 4f64);
+        assert_eq!(sum.z, 4f64);
     }
 }
