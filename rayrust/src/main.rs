45a13e3014002bb282529564e7f41f20ea09efd3
YruamaLairba <Yruama_Lairba@hotmail.com>
2017-11-12 20:35:18 +0100

    implement render() (untested)

--- rayrust/src/main.rs
+++ rayrust/src/main.rs
@@ -2,6 +2,8 @@ extern crate rayrust;
 use rayrust::{V3, solvetri, Sol};
 
 use std::f64;
+use std::fs::File;
+use std::io::prelude::*;
 
 struct Sphere {
     center: V3,
@@ -39,10 +41,10 @@ fn intersec( sph1: &Sphere,  o: &V3,  v: &V3) -> f64 {
 
 fn trace( o: &V3, v: &V3) -> [f64; 3] {
     const TMAX:f64 = 1E10f64;
-	let mut tmin = f64::INFINITY;
-	let mut omin = [0f64;3];
-	//unsigned imin = 0;
-	for elem in SPH.iter() {
+    let mut tmin = f64::INFINITY;
+    let mut omin = [0f64;3];
+    //unsigned imin = 0;
+    for elem in SPH.iter() {
         let tres = intersec( elem, o, v);
         if (tres > 0f64) && (tres < tmin) {
             tmin = tres;
@@ -54,6 +56,53 @@ fn trace( o: &V3, v: &V3) -> [f64; 3] {
     }
     return omin;
 }
+
+fn render( w: u32, h: u32, fnameout: String) {
+// screen
+    let m_ww = 1f64;
+    let m_hh = m_ww * (h as f64) / (w as f64);
+    // camera
+    let cam = [
+        V3 { x: 0.4, y: 0.0, z: 0.4},               // eye
+        V3 { x: -1.0, y: 0.0, z: -1.0},             // front towards screen
+        V3 { x: -0.707107, y: 0.0, z: 0.707107},    // up along screen
+    ];
+    let m_e = cam[0].clone();   // eye position
+    let m_f = cam[1].clone();   // front towards screen
+    let mut m_u = cam[2].clone();   // up along screen
+    //v3 m_r;   // right along screen (computed)
+
+    m_u.normalize();
+    let mut m_r = V3::cross(&m_f, &m_u);      // compute right
+    m_u = V3::cross(&m_r, &m_f);      // re-compute up
+    m_u.normalize();
+    m_r.normalize();
+    let mut fout = File::create(fnameout).expect("file not found");
+    let _ = write!(fout, "P3\n");
+    let _ = write!(fout,"{} {}\n", w, h);
+    let _ = write!(fout,"{}\n", 100);
+
+    //let mut v = V3 {x: 0f64, y: 0f64, z: 0f64};
+    for jj in 0..h {
+        let vu = &m_u * (((h as f64) - (jj as f64) - 1f64 - (h as f64)/ 2.0) / 
+            (h as f64) * m_hh);
+//      vprintn( "rayc vu", vu);
+//      getchar();
+        for ii in 0..w {
+            let vr = &m_r * (((ii as f64) - (w as f64) / 2.0) / (w as f64) 
+                * m_ww);
+            let mut v = &vu + &vr;
+            v = &v + &m_f;
+            v.normalize();
+            let color = trace( &m_e, &v);
+            let _ = write!(fout, "{} {} {}   ", 100 * (color[0] as u8),
+                100 * (color[1] as u8), 100 * (color[2] as u8));
+        }
+        let _ = write!(fout, "\n");
+    }
+    // need to flush here ?
+}
+
 
 fn main() {
     println!("Hello, world!");
