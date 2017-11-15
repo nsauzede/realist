a31465760893ab9679fca5b7b742ac4e7425f58c
YruamaLairba <YruamaLairba@users.noreply.github.com>
2017-11-15 19:16:31 +0100

    Rust (#1)
    * init rust project + cross implementation
    
    * implement add trait for V3
    
    * implment Sub trait for V3
    
    * derive Clone trait to implement clone() method
    
    * implement V3 * f64 and f64 * V3
    
    * implement Add, Sub, Mul using ref
    
    * implement dot()
    
    * implement normalize()
    
    * make V3 member public
    
    * implment solvetri using custom enum (untested)
    
    * first implmentation of interect, adjust sph cst
    
    * implement Trace() (untested)
    
    * fix naming convention of function
    
    * implement render() (untested)
    
    * implement main(), it compile but doesn't work...
    
    image is black
    
    * Derive debug trait on V3
    
    * fix an error in trace()
    
    * fix error when writing value to file
    
    * oops forget to remove "hello world"

---
+++ rayrust/src/lib.rs
@@ -0,0 +1,275 @@
+use std::ops::{Add,Sub,Mul};
+
+#[derive(Clone)]
+#[derive(Debug)]
+pub struct V3 {
+   pub x: f64,
+   pub y: f64,
+   pub z: f64,
+}
+
+impl V3 {
+    pub fn cross(&self, other: &V3) -> V3 {
+        V3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    pub fn dot(&self, other: &V3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn normalize(&mut self) {
+        let norm = f64::sqrt(V3::dot(&self, &self));
+        self.x /= norm;
+        self.y /= norm;
+        self.z /= norm;
+    }
+}
+
+impl Add for V3 {
+    type Output = V3;
+    fn add(self, other: V3) -> V3 {
+        V3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl<'a,'b> Add<&'b V3> for &'a V3 {
+    type Output = V3;
+    fn add(self, other: &V3) -> V3 {
+        V3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+
+impl Sub for V3 {
+    type Output = V3;
+    fn sub(self, other: V3) -> V3 {
+        V3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl<'a, 'b> Sub<&'b V3> for &'a V3 {
+    type Output = V3;
+    fn sub(self, other: &V3) -> V3 {
+        V3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul<f64> for V3 {
+    type Output = V3;
+    fn mul(self, other: f64) -> V3 {
+        V3 {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl<'b> Mul<f64> for &'b V3 {
+    type Output = V3;
+    fn mul(self, other: f64) -> V3 {
+        V3 {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl Mul<V3> for f64 {
+    type Output = V3;
+    fn mul(self, other: V3) -> V3 {
+        V3 {
+            x: self * other.x,
+            y: self * other.y,
+            z: self * other.z,
+        }
+    }
+}
+
+impl<'a> Mul<&'a V3> for f64 {
+    type Output = V3;
+    fn mul(self, other: &V3) -> V3 {
+        V3 {
+            x: self * other.x,
+            y: self * other.y,
+            z: self * other.z,
+        }
+    }
+}
+
+pub enum Sol {
+    S2(f64, f64),
+    S1(f64),
+    None,
+}
+
+pub fn solvetri(a: f64, b: f64, c:f64) -> Sol {
+    let d = b * b - 4f64 * a * c;
+    if d > 0f64 {
+        let sd = f64::sqrt( d);
+        let t1 = (-b - sd) / 2f64 / a;
+        let t2 = (-b + sd) / 2f64 / a;
+        return Sol::S2(t1, t2);
+    } else if d == 0f64 {
+        let t1 = -b / 2f64 / a;
+        return Sol::S1(t1)
+    } else {
+        return Sol::None;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cross() {
+        let a =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let b =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let cross = V3::cross(&a,&b);
+        assert_eq!(cross.x, 0f64);
+        assert_eq!(cross.y, 0f64);
+        assert_eq!(cross.z, 0f64);
+        //test if input moves
+        drop(a);
+        drop(b);
+    }
+
+    #[test]
+    fn test_dot() {
+        let a =  V3 {x: 1f64, y: 2f64, z: 2f64};
+        let b =  V3 {x: 2f64, y: 2f64, z: 3f64};
+        let res = V3::dot(&a,&b);
+        assert_eq!(res, 12f64);
+        //test if input moves
+        drop(a);
+        drop(b);
+    }
+
+    #[test]
+    fn test_normalize() {
+        let mut a =  V3 {x: 16f64, y: 0f64, z: 0f64};
+        a.normalize();
+        assert_eq!(a.x, 1f64);
+        //test if input moves
+        drop(a);
+    }
+
+    #[test]
+    fn test_add_func() {
+        let a =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let b =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let sum = <&V3 as Add<&V3>>::add(&a,&b);
+        assert_eq!(sum.x, 4f64);
+        assert_eq!(sum.y, 4f64);
+        assert_eq!(sum.z, 4f64);
+    }
+
+    #[test]
+    fn test_add_val() {
+        let a =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let b =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let sum = a + b;
+        assert_eq!(sum.x, 4f64);
+        assert_eq!(sum.y, 4f64);
+        assert_eq!(sum.z, 4f64);
+    }
+
+    #[test]
+    fn test_add_ref() {
+        let a =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let b =  V3 {x: 2f64, y: 2f64, z: 2f64};
+        let sum = &a + &b;
+        assert_eq!(sum.x, 4f64);
+        assert_eq!(sum.y, 4f64);
+        assert_eq!(sum.z, 4f64);
+    }
+
+    #[test]
+    fn test_sub_val() {
+        let a =  V3 {x: 4f64, y: 4f64, z: 4f64};
+        let b =  V3 {x: 3f64, y: 2f64, z: 1f64};
+        let res = a - b;
+        assert_eq!(res.x, 1f64);
+        assert_eq!(res.y, 2f64);
+        assert_eq!(res.z, 3f64);
+    }
+
+    #[test]
+    fn test_sub_ref() {
+        let a =  V3 {x: 4f64, y: 4f64, z: 4f64};
+        let b =  V3 {x: 3f64, y: 2f64, z: 1f64};
+        let res = &a - &b;
+        assert_eq!(res.x, 1f64);
+        assert_eq!(res.y, 2f64);
+        assert_eq!(res.z, 3f64);
+    }
+
+    #[test]
+    fn test_mul_val1() {
+        let a =  V3 {x: 1f64, y: 2f64, z: 3f64};
+        let res = a * 5f64;
+        assert_eq!(res.x, 5f64);
+        assert_eq!(res.y, 10f64);
+        assert_eq!(res.z, 15f64);
+    }
+
+    #[test]
+    fn test_mul_ref1() {
+        let a =  V3 {x: 1f64, y: 2f64, z: 3f64};
+        let res = &a * 5f64;
+        assert_eq!(res.x, 5f64);
+        assert_eq!(res.y, 10f64);
+        assert_eq!(res.z, 15f64);
+    }
+
+    #[test]
+    fn test_mul_val2() {
+        let a =  V3 {x: 1f64, y: 2f64, z: 3f64};
+        let res = 5f64 * a;
+        assert_eq!(res.x, 5f64);
+        assert_eq!(res.y, 10f64);
+        assert_eq!(res.z, 15f64);
+    }
+
+    #[test]
+    fn test_mul_ref2() {
+        let a =  V3 {x: 1f64, y: 2f64, z: 3f64};
+        let res = 5f64 * &a;
+        assert_eq!(res.x, 5f64);
+        assert_eq!(res.y, 10f64);
+        assert_eq!(res.z, 15f64);
+    }
+
+    #[test]
+    fn test_clone() {
+        let a =  V3 {x: 1f64, y: 2f64, z: 3f64};
+        let res = a.clone();
+        drop(a);
+        assert_eq!(res.x, 1f64);
+        assert_eq!(res.y, 2f64);
+        assert_eq!(res.z, 3f64);
+    }
+}
---
+++ rayrust/src/main.rs
@@ -0,0 +1,121 @@
+extern crate rayrust;
+use rayrust::{V3, solvetri, Sol};
+
+use std::f64;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+struct Sphere {
+    center: V3,
+    radius: f64,
+    color: [f64;3],
+}
+
+const SPH:[Sphere;9] = [
+    Sphere { center: V3 {x: 0.0 , y:-0.1 , z: 0.0} , radius: 0.05, color: [0.8, 0.8, 0.8]},
+    Sphere { center: V3 {x: 0.0 , y: 0.0 , z: 0.0} , radius: 0.05, color: [0.8, 0.8, 0.8]},
+    Sphere { center: V3 {x: 0.0 , y: 0.1 , z: 0.0} , radius: 0.05, color: [0.8, 0.8, 0.8]},
+    Sphere { center: V3 {x: 0.1 , y:-0.05, z: 0.0} , radius: 0.05, color: [0.8, 0.0, 0.0]},
+    Sphere { center: V3 {x: 0.1 , y: 0.05, z: 0.0} , radius: 0.05, color: [0.0, 0.0, 0.8]},
+    Sphere { center: V3 {x: 0.2 , y: 0.0 , z: 0.0} , radius: 0.05, color: [0.0, 0.8, 0.0]},
+    Sphere { center: V3 {x: 0.05, y:-0.05, z: 0.1} , radius: 0.05, color: [0.8, 0.8, 0.8]},
+    Sphere { center: V3 {x: 0.05, y: 0.05, z: 0.1} , radius: 0.05, color: [0.8, 0.8, 0.8]},
+    Sphere { center: V3 {x: 0.0 , y:-0.5 , z: 0.5} , radius: 0.02, color: [1.0, 1.0, 0.0]},
+];
+
+fn intersec( sph1: &Sphere,  o: &V3,  v: &V3) -> f64 {
+    let m_r = sph1.radius;
+    let m_c = &sph1.center;
+    let sr2 = m_r * m_r;
+    let vt =  o - m_c;//V3::sub(o, m_c);
+    let a = V3::dot( v, v);
+    let b = 2f64 * V3::dot( v, &vt);
+    let c = V3::dot( &vt, &vt) - sr2;
+    let sol = solvetri( a, b, c);
+    match sol {
+        Sol::S2(t1, t2) => {return f64::max(t1, t2);},
+        Sol::S1(t) => {return t;},
+        Sol::None => {return f64::INFINITY;},
+    }
+}
+
+fn trace( o: &V3, v: &V3) -> [f64; 3] {
+    const TMAX:f64 = 1E10f64;
+    let mut tmin = f64::INFINITY;
+    let mut omin = [0f64;3];
+    //unsigned imin = 0;
+    for elem in SPH.iter() {
+        let tres = intersec( elem, o, v);
+        if (tres > 0f64) && (tres < tmin) {
+            tmin = tres;
+            omin = elem.color;
+        }
+    }
+    if tmin > TMAX {
+        omin = [0f64;3];
+    }
+    return omin;
+}
+
+fn render( w: u32, h: u32, fnameout: String) {
+// screen
+    let m_ww = 1f64;
+    let m_hh = m_ww * (h as f64) / (w as f64);
+    // camera
+    let cam = [
+        V3 { x: 0.4, y: 0.0, z: 0.4},               // eye
+        V3 { x: -1.0, y: 0.0, z: -1.0},             // front towards screen
+        V3 { x: -0.707107, y: 0.0, z: 0.707107},    // up along screen
+    ];
+    let m_e = cam[0].clone();   // eye position
+    let m_f = cam[1].clone();   // front towards screen
+    let mut m_u = cam[2].clone();   // up along screen
+    //v3 m_r;   // right along screen (computed)
+
+    m_u.normalize();
+    let mut m_r = V3::cross(&m_f, &m_u);      // compute right
+    m_u = V3::cross(&m_r, &m_f);      // re-compute up
+    m_u.normalize();
+    m_r.normalize();
+    let mut fout = File::create(fnameout).expect("file not found");
+    let _ = write!(fout, "P3\n");
+    let _ = write!(fout,"{} {}\n", w, h);
+    let _ = write!(fout,"{}\n", 100);
+
+    //let mut v = V3 {x: 0f64, y: 0f64, z: 0f64};
+    for jj in 0..h {
+        let vu = &m_u * (((h as f64) - (jj as f64) - 1f64 - (h as f64)/ 2.0) / 
+            (h as f64) * m_hh);
+//      vprintn( "rayc vu", vu);
+//      getchar();
+        for ii in 0..w {
+            let vr = &m_r * (((ii as f64) - (w as f64) / 2.0) / (w as f64) 
+                * m_ww);
+            let mut v = &vu + &vr;
+            v = &v + &m_f;
+            v.normalize();
+            let color = trace( &m_e, &v);
+            let _ = write!(fout, "{:3.0} {:3.0} {:3.0}   ", 100.0 * color[0],
+                100.0 * color[1], 100.0 * color[2]);
+        }
+        let _ = write!(fout, "\n");
+    }
+    // need to flush here ?
+}
+
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+    let w: u32 = args.next()
+                     .unwrap()
+                     .parse()
+                     .expect("width missing or incorrect");
+    let h: u32 = args.next()
+                     .unwrap()
+                     .parse()
+                     .expect("height missing or incorrect");
+    let file_name = args.next().expect("require file output");
+    render(w, h, file_name);
+}
