c2347a21e914738a93b760234e7022b63fe1b57c
Nicolas Sauzede <nsauzede@laposte.net>
2020-01-03 22:14:30 +0100

    Add light emitting material and missing picture

--- rtiow/rttnw/hittable.h
+++ rtiow/rttnw/hittable.h
@@ -205,6 +205,9 @@ class material  {
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, vec3& attenuation,
             ray& scattered) const = 0;
+        virtual vec3 emitted(float u, float v, const vec3& p) const {
+            return vec3(0,0,0);
+        }
 };
 
 #endif
