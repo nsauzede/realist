8521fe29093c5239df0b19cf3346ecc092567f61
Nicolas Sauzede <nsauzede@laposte.net>
2020-01-08 23:21:30 +0100

    Add missing random.h; update README.md

--- rtiow/rttroyl/random.h
+++ rtiow/rttroyl/random.h
@@ -16,4 +16,12 @@ vec3 random_in_unit_sphere() {
     return p;
 }
 
+vec3 random_on_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(random_double(),random_double(),random_double()) - vec3(1,1,1);
+    } while (dot(p,p) >= 1.0);
+    return unit_vector(p);
+}
+
 #endif
