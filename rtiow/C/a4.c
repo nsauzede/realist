e268c1313f30199de3171646c749c02f7192dd73
Nicolas Sauzede <nsauzede@laposte.net>
2020-01-22 23:54:15 +0100

    Add a4.c main4.cpp and align outputs; add A5.v

---
+++ rtiow/C/a4.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "vec3.h"
+#include "ray.h"
+
+void color(vec3 col, const ray r) {
+	vec3 unit_direction;
+	unit_vector(unit_direction, r.direction);
+	float t = 0.5*(unit_direction[1] + 1.0);
+	vec3 col0 = {1.0, 1.0, 1.0};
+	vec3 col1 = {0.5, 0.7, 1.0};
+	vmul(col0, 1.0-t, col0);
+	vmul(col1, t, col1);
+	vadd(col, col0, col1);
+}
+
+int main() {
+	int nx = 200;
+	int ny = 100;
+	printf("P3\n"); printf("%d %d\n", nx, ny); printf("255\n");
+	vec3 lower_left_corner = {-2.0, -1.0, -1.0};
+//	vprint(lower_left_corner);
+	vec3 horizontal = {4.0, 0.0, 0.0};
+	vec3 vertical = {0.0, 2.0, 0.0};
+	vec3 origin = {0.0, 0.0, 0.0};
+	for (int j = ny-1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float u = (float)i / (float)nx;
+			float v = (float)j / (float)ny;
+			ray r;
+			vec3 direction, direction0, direction1;
+			vmul(direction0, v, vertical);
+			vmul(direction1, u, horizontal);
+			vadd(direction, direction0, direction1);
+			vadd(direction, direction, lower_left_corner);
+			rmake(&r, origin, direction);
+//			rprint(&r);exit(0);
+			vec3 col;
+			color(col, r);
+			int ir = (int)(255.99*col[0]);
+			int ig = (int)(255.99*col[1]);
+			int ib = (int)(255.99*col[2]);
+			printf("%d %d %d\n", ir, ig, ib);
+		}
+	}
+}
--- rtiow/C/vec3.h
+++ rtiow/C/vec3.h
@@ -1,6 +1,50 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+#include <math.h>
+
 typedef float vec3[3];
+
+#define API static inline
+
+API void vprint(const vec3 r) {
+	printf("{%f, %f, %f}", r[0], r[1], r[2]);
+}
+
+API void vcopy(vec3 l, const vec3 r) {
+	l[0] = r[0];
+	l[1] = r[1];
+	l[2] = r[2];
+}
+
+API float vsqlen(const vec3 r) {
+	return r[0] * r[0] + r[1] * r[1] + r[2] * r[2];
+}
+
+API float vlen(const vec3 r) {
+	return sqrt(vsqlen(r));
+}
+
+API void vadd(vec3 l, const vec3 r1, const vec3 r2) {
+	l[0] = r1[0] + r2[0];
+	l[1] = r1[1] + r2[1];
+	l[2] = r1[2] + r2[2];
+}
+
+API void vmul(vec3 l, float r1, const vec3 r2) {
+	l[0] = r1 * r2[0];
+	l[1] = r1 * r2[1];
+	l[2] = r1 * r2[2];
+}
+
+API void vdiv(vec3 l, const vec3 r1, float r2) {
+	l[0] = r1[0] / r2;
+	l[1] = r1[1] / r2;
+	l[2] = r1[2] / r2;
+}
+
+API void unit_vector(vec3 l, const vec3 r) {
+	vdiv(l, r, vlen(r));
+}
 
 #endif/*VEC3_H*/
