7c5d799373b01b20316223f3a1db5377ac86b0e1
Delyan Angelov <delian66@gmail.com>
2021-02-02 20:36:10 +0200

    fix compilation of ray_v/rayv.v with V 0.2.2 91af241

--- ray_v/rayv.v
+++ ray_v/rayv.v
@@ -1,15 +1,13 @@
-/*
- * Copyright(c) 2016-2019 Nicolas Sauzede (nsauzede@laposte.net)
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
 module main
 
-import (
-	vec
-	os
-	math
-	strings
-)
+/*
+* Copyright(c) 2016-2019 Nicolas Sauzede (nsauzede@laposte.net)
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+import vec
+import os
+import math
+import strings
 
 struct RgbColor {
 	r f64
@@ -18,7 +16,7 @@ struct RgbColor {
 }
 
 struct Thing {
-	points []f64
+	points [7]f64
 }
 
 struct Scene {
@@ -27,16 +25,12 @@ mut:
 }
 
 const (
-	HUGE_VAL = f64(C.HUGE_VAL)
-	math_MaxFloat64 = 1.797693134862315708145274237317043567981e+308
-	init_t12 = [f64(0.)].repeat(2)
-	init_omin = [f64(0.)].repeat(7) // WARNING : needs to be big enough for any obj
-	init_space = ' '
-	black_color = RgbColor {
-		f64(0.),f64(0.),f64(0.)}
+	huge_val    = f64(C.HUGE_VAL)
+	init_space  = ' '
+	black_color = RgbColor{0, 0, 0}
 )
 
-fn solve_tri(a, b, c f64, t mut []f64) int {
+fn solve_tri(a f64, b f64, c f64, mut t [2]f64) int {
 	mut sol := 0
 	d := b * b - f64(4.) * a * c
 	if d > 0 {
@@ -44,87 +38,70 @@ fn solve_tri(a, b, c f64, t mut []f64) int {
 		t[0] = (-b - sd) / 2 / a
 		t[1] = (-b + sd) / 2 / a
 		sol = 2
-	}
-	else if d == 0 {
+	} else if d == 0 {
 		t[0] = -b / 2 / a
 		sol = 1
 	}
 	return sol
 }
 
-fn intersec(s &f64, o, v vec.Vector) f64 {
-	mut t := HUGE_VAL
-	center := vec.Vector {
-		s[0],s[1],s[2]}
-	rad := s[3]
+fn intersec(s &f64, o vec.Vector, v vec.Vector) f64 {
+	mut t := huge_val
+	center := unsafe { vec.Vector{s[0], s[1], s[2]} }
+	rad := unsafe { s[3] }
 	vt := o.sub(center)
 	a := v.dot(v)
 	b := f64(2.) * v.dot(vt)
 	c := vt.dot(vt) - rad * rad
-	mut t12 := init_t12
+	mut t12 := [2]f64{}
 	sol := solve_tri(a, b, c, mut t12)
 	if sol == 2 {
 		if t12[0] < t12[1] {
 			t = t12[0]
-		}
-		else {
+		} else {
 			t = t12[1]
 		}
-	}
-	else if sol == 1 {
+	} else if sol == 1 {
 		t = t12[0]
 	}
 	return t
 }
 
-fn trace(scene &Scene, o, v vec.Vector) RgbColor {
-	mut tmin := HUGE_VAL
-	mut omin := init_omin
+fn trace(scene &Scene, o vec.Vector, v vec.Vector) RgbColor {
+	mut tmin := huge_val
+	mut omin := [7]f64{}
 	for s in scene.objects {
-		t := intersec(s.points.data, o, v)
+		t := intersec(&f64(s.points), o, v)
 		if t > 0 && t < tmin {
 			tmin = t
-			omin = s.points
+			unsafe { C.memcpy(omin, s.points, sizeof(omin)) }
 		}
 	}
-	if tmin < HUGE_VAL {
-		return RgbColor {
-			omin[4],omin[5],omin[6]}
+	if tmin < huge_val {
+		return RgbColor{omin[4], omin[5], omin[6]}
 	}
 	return black_color
 }
 
-fn render(w, h int, fnameout string) {
-	ratiox := f64(1.)
-	ratioy := f64(1.)
+fn render(w int, h int, fnameout string) ? {
+	ratiox := f64(1.0)
+	ratioy := f64(1.0)
 	ww := ratiox
 	hh := ww * f64(h) / f64(w) * ratioy
-	e := vec.Vector {
-		f64(0.4),f64(0),f64(0.4)}
-	/*mut*/
-	f := vec.Vector {
-		f64(-1),f64(0),f64(-1)}
-	mut u := vec.Vector {
-		f64(-0.707107),f64(0),f64(0.707107)}
+	e := vec.Vector{0.4, 0, 0.4}
+	// mut
+	f := vec.Vector{-1, 0, -1}
+	mut u := vec.Vector{-0.707107, 0, 0.707107}
 	mut scene := Scene{}
-	scene.objects << Thing{
-		[f64(0), f64(-0.1), f64(0), f64(0.05), f64(0.8), f64(0.8), f64(0.8)]}
-	scene.objects << Thing{
-		[f64(0), f64(0), f64(0), f64(0.05), f64(0.8), f64(0.8), f64(0.8)]}
-	scene.objects << Thing{
-		[f64(0), f64(0.1), f64(0), f64(0.05), f64(0.8), f64(0.8), f64(0.8)]}
-	scene.objects << Thing{
-		[f64(0.1), f64(-0.05), f64(0), f64(0.05), f64(0.8), f64(0), f64(0)]}
-	scene.objects << Thing{
-		[f64(0.1), f64(0.05), f64(0), f64(0.05), f64(0), f64(0), f64(0.8)]}
-	scene.objects << Thing{
-		[f64(0.2), f64(0), f64(0), f64(0.05), f64(0), f64(0.8), f64(0)]}
-	scene.objects << Thing{
-		[f64(0.05), f64(-0.05), f64(0.1), f64(0.05), f64(0.8), f64(0.8), f64(0.8)]}
-	scene.objects << Thing{
-		[f64(0.05), f64(0.05), f64(0.1), f64(0.05), f64(0.8), f64(0.8), f64(0.8)]}
-	scene.objects << Thing{
-		[f64(0), f64(-0.5), f64(0.5), f64(0.02), f64(1), f64(1), f64(0)]}
+	scene.objects << Thing{[f64(0), -0.1, 0, 0.05, 0.8, 0.8, 0.8]!}
+	scene.objects << Thing{[f64(0), 0, 0, 0.05, 0.8, 0.8, 0.8]!}
+	scene.objects << Thing{[f64(0), 0.1, 0, 0.05, 0.8, 0.8, 0.8]!}
+	scene.objects << Thing{[f64(0.1), -0.05, 0, 0.05, 0.8, 0, 0]!}
+	scene.objects << Thing{[f64(0.1), 0.05, 0, 0.05, 0, 0, 0.8]!}
+	scene.objects << Thing{[f64(0.2), 0, 0, 0.05, 0, 0.8, 0]!}
+	scene.objects << Thing{[f64(0.05), -0.05, 0.1, 0.05, 0.8, 0.8, 0.8]!}
+	scene.objects << Thing{[f64(0.05), 0.05, 0.1, 0.05, 0.8, 0.8, 0.8]!}
+	scene.objects << Thing{[f64(0), -0.5, 0.5, 0.02, 1, 1, 0]!}
 	u.normalize()
 	mut r := f.cross(u)
 	u = r.cross(f)
@@ -134,17 +111,19 @@ fn render(w, h int, fnameout string) {
 	// we create the output file unconditionally, before checking if fnameout
 	// is empty. This is due to a bug where mut os.File reassigned by os.create
 	// in sub-block seems to be wiped at block exit (??)
-//	mut fout := os.create(fnameout) or { exit }
-	mut fout := os.create(fnameout) or { os.File{cfile: 0} }
+	//	mut fout := os.create(fnameout) or { exit }
+	mut fout := os.create(fnameout) or { os.File{
+		cfile: 0
+	} }
 	mut bytes := byteptr(0)
 	mut nbytes := 0
 	mut picture_string := strings.new_builder(1024)
 	if fnameout != '' {
-		fout.writeln('P6')
+		fout.writeln('P6') ?
 		nbytes = 3 * h * w
 		bytes = malloc(nbytes)
-		fout.writeln('$w $h')
-		fout.writeln('$max')
+		fout.writeln('$w $h') ?
+		fout.writeln('$max') ?
 	} else {
 		picture_string.writeln('P3')
 		picture_string.writeln('$w $h')
@@ -161,9 +140,11 @@ fn render(w, h int, fnameout string) {
 			gg := int(f64(max) * rgb.g)
 			bb := int(f64(max) * rgb.b)
 			if fnameout != '' {
-				bytes[(j * w + i) * 3 + 0] = rr
-				bytes[(j * w + i) * 3 + 1] = gg
-				bytes[(j * w + i) * 3 + 2] = bb
+				unsafe {
+					bytes[(j * w + i) * 3 + 0] = byte(rr)
+					bytes[(j * w + i) * 3 + 1] = byte(gg)
+					bytes[(j * w + i) * 3 + 2] = byte(bb)
+				}
 			} else {
 				picture_string.write('${rr:2d} ${gg:2d} ${bb:2d}   ')
 			}
@@ -201,5 +182,5 @@ fn main() {
 			}
 		}
 	}
-	render(w, h, fnameout)
+	render(w, h, fnameout) ?
 }
--- ray_v/vec/vec.v
+++ ray_v/vec/vec.v
@@ -1,72 +1,52 @@
-/*
- * Copyright(c) 2016-2019 Nicolas Sauzede (nsauzede@laposte.net)
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
 module vec
 
+/*
+* Copyright(c) 2016-2019 Nicolas Sauzede (nsauzede@laposte.net)
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
 import math
 
 pub struct Vector {
-
-        x f64
-        y f64
-        z f64
-
-//        x, y, z f64
+	x f64
+	y f64
+	z f64
+	//        x, y, z f64
 }
 
 pub fn (v Vector) str() string {
-        return '[${v.x}, ${v.y}, ${v.z}]'
+	return '[$v.x, $v.y, $v.z]'
 }
 
 pub fn (v Vector) dot(ov Vector) f64 {
-        return v.x * ov.x + v.y * ov.y + v.z * ov.z
+	return v.x * ov.x + v.y * ov.y + v.z * ov.z
 }
 
 pub fn (v Vector) cross(ov Vector) Vector {
-        return Vector {
-                v.y * ov.z - v.z * ov.y,
-                v.z * ov.x - v.x * ov.z,
-                v.x * ov.y - v.y * ov.x
-        }
+	return Vector{v.y * ov.z - v.z * ov.y, v.z * ov.x - v.x * ov.z, v.x * ov.y - v.y * ov.x}
 }
 
 pub fn (v Vector) norm() f64 {
-        return math.sqrt(v.dot(v))
+	return math.sqrt(v.dot(v))
 }
 
 pub fn (v Vector) mult(f f64) Vector {
-        return Vector {
-                v.x * f,
-                v.y * f,
-                v.z * f
-        }
+	return Vector{v.x * f, v.y * f, v.z * f}
 }
 
 pub fn (v Vector) div(f f64) Vector {
-        return Vector {
-                v.x / f,
-                v.y / f,
-                v.z / f
-        }
+	return Vector{v.x / f, v.y / f, v.z / f}
 }
 
-pub fn (v mut Vector) normalize() {
-        *v = v.div(v.norm())
+pub fn (mut v Vector) normalize() {
+	unsafe {
+		*v = v.div(v.norm())
+	}
 }
 
 pub fn (v Vector) add(ov Vector) Vector {
-        return Vector {
-                v.x + ov.x,
-                v.y + ov.y,
-                v.z + ov.z
-        }
+	return Vector{v.x + ov.x, v.y + ov.y, v.z + ov.z}
 }
 
 pub fn (v Vector) sub(ov Vector) Vector {
-        return Vector {
-                v.x - ov.x,
-                v.y - ov.y,
-                v.z - ov.z
-        }
+	return Vector{v.x - ov.x, v.y - ov.y, v.z - ov.z}
 }
