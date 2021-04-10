fb9873725fe88326f6f8aafacf43daee48414608
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-10 11:45:33 +0200

    Null decimal part is useless

--- rtiow/Nim/main04.nim
+++ rtiow/Nim/main04.nim
@@ -4,18 +4,18 @@ import ray
 
 func color(r:Ray): Vec3 =
  var unit_direction = unit_vector(r.direction)
- var t = 0.5 * (unit_direction.y + 1.0)
- result = (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0)
+ var t = 0.5 * (unit_direction.y + 1)
+ result = (1 - t) * vec3(1, 1, 1) + t * vec3(0.5, 0.7, 1)
 
 var nx = 200
 var ny = 100
 echo("P3")
 echo(fmt"{nx} {ny}")
 echo(255)
-var lower_left_corner = vec3(-2.0, -1.0, -1.0)
-var horizontal = vec3(4.0, 0.0, 0.0)
-var vertical = vec3(0.0, 2.0, 0.0)
-var origin = vec3(0.0, 0.0, 0.0)
+var lower_left_corner = vec3(-2, -1, -1)
+var horizontal = vec3(4, 0, 0)
+var vertical = vec3(0, 2, 0)
+var origin = vec3(0, 0, 0)
 for j in countdown(ny - 1, 0):
  for i in countup(0, nx - 1):
   var u = float32(i) / float32(nx)
