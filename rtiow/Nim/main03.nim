48f05c1e05c1fc97f04fd980b2bb9b0da25b06d6
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-10 11:42:38 +0200

    Simplify syntax

--- rtiow/Nim/main03.nim
+++ rtiow/Nim/main03.nim
@@ -7,7 +7,7 @@ echo(fmt"{nx} {ny}")
 echo(255)
 for j in countdown(ny - 1, 0):
  for i in countup(0, nx - 1):
-  var col : Vec3 = Vec3(x:i / nx, y:j / ny, z:0.2)
+  var col = vec3(i / nx, j / ny, 0.2)
   var ir = int(255.99 * col.x)
   var ig = int(255.99 * col.y)
   var ib = int(255.99 * col.z)
