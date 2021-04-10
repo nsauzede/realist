b74f6aba70ee53107d370f5a87493c8920169f7b
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-10 12:27:05 +0200

    Add main06_1.nim

--- rtiow/Nim/ray.nim
+++ rtiow/Nim/ray.nim
@@ -7,3 +7,6 @@ type
 func ray*(origin: Vec3, direction: Vec3): Ray {.inline.} =
  result.origin = origin
  result.direction = direction
+
+func point_at_parameter*(r: Ray, t: float32): Vec3 =
+ r.origin + t * r.direction
