c8cbe7d88c5d2aa2a534887e921be740c237cf66
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-10 11:42:38 +0200

    Simplify syntax

--- rtiow/Nim/main04.nim
+++ rtiow/Nim/main04.nim
@@ -5,17 +5,17 @@ import ray
 func color(r:Ray): Vec3 =
  var unit_direction = unit_vector(r.direction)
  var t = 0.5 * (unit_direction.y + 1.0)
- result = (1.0 - t) * Vec3(x: 1.0, y: 1.0, z: 1.0) + t * Vec3(x: 0.5, y: 0.7, z: 1.0)
+ result = (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0)
 
 var nx = 200
 var ny = 100
 echo("P3")
 echo(fmt"{nx} {ny}")
 echo(255)
-var lower_left_corner = Vec3(x: -2.0, y: -1.0, z: -1.0)
-var horizontal = Vec3(x: 4.0, y: 0.0, z: 0.0)
-var vertical = Vec3(x: 0.0, y: 2.0, z: 0.0)
-var origin = Vec3(x: 0.0, y: 0.0, z: 0.0)
+var lower_left_corner = vec3(-2.0, -1.0, -1.0)
+var horizontal = vec3(4.0, 0.0, 0.0)
+var vertical = vec3(0.0, 2.0, 0.0)
+var origin = vec3(0.0, 0.0, 0.0)
 for j in countdown(ny - 1, 0):
  for i in countup(0, nx - 1):
   var u = float32(i) / float32(nx)
