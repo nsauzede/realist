6eab6b1154473ec053146a4d5f48b0fb1320170f
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-11 05:10:01 +0200

    Add main07.nim

--- rtiow/Nim/vec3.nim
+++ rtiow/Nim/vec3.nim
@@ -1,37 +1,36 @@
 import math
 
-type
- Vec3* = object
-   x*, y*, z*: float32
+type Vec3* = object
+    x*, y*, z*: float32
 
 func vec3*(x, y, z: float32): Vec3 {.inline.} =
- Vec3(x: x, y: y, z: z)
+    Vec3(x: x, y: y, z: z)
 
 func length*(v: Vec3): float32 {.inline.} =
- sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+    sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
 
 func dot*(v1, v2: Vec3): float32 {.inline.} =
- v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
 
 func `/`*(v: Vec3, t: float32): Vec3 {.inline.} =
- result.x = v.x / t
- result.y = v.y / t
- result.z = v.z / t
+    result.x = v.x / t
+    result.y = v.y / t
+    result.z = v.z / t
 
 func `*`*(t: float32, v: Vec3): Vec3 {.inline.} =
- result.x = t * v.x
- result.y = t * v.y
- result.z = t * v.z
+    result.x = t * v.x
+    result.y = t * v.y
+    result.z = t * v.z
 
 func `+`*(v1: Vec3, v2: Vec3): Vec3 {.inline.} =
- result.x = v1.x + v2.x
- result.y = v1.y + v2.y
- result.z = v1.z + v2.z
+    result.x = v1.x + v2.x
+    result.y = v1.y + v2.y
+    result.z = v1.z + v2.z
 
 func `-`*(v1: Vec3, v2: Vec3): Vec3 {.inline.} =
- result.x = v1.x - v2.x
- result.y = v1.y - v2.y
- result.z = v1.z - v2.z
+    result.x = v1.x - v2.x
+    result.y = v1.y - v2.y
+    result.z = v1.z - v2.z
 
 func unit_vector*(v: Vec3): Vec3 {.inline.} =
- v / v.length()
+    v / v.length()
